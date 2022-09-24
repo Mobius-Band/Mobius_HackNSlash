1d533e37099725145e89da66311862f38291223b
Marlus Vinicius <marlus.cabral@compasso.com.br>
2022-09-23 23:05:15 -0300

    Enemy attack when close to player

--- Assets/HackNSlash/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/HackNSlash/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,23 +1,37 @@
 using System;
 using System.Collections;
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyMovement : MonoBehaviour
+public class EnemyBehaviour : MonoBehaviour
 {
     [SerializeField] private Transform _player;
     [SerializeField] private float _chasePeriod = 1f;
+    [SerializeField] private float _attackEnablingAngle = 30f;
 
     private NavMeshAgent _navMeshAgent;
+    private Animator _animator;
+
+    private bool _canAttack = false;
 
     void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
     }
 
     void Start()
     {
         StartCoroutine(CheckForTarget());
+    }
+
+    private void Update()
+    {
+        _canAttack = Vector3.SqrMagnitude(_player.position - transform.position) <=
+                      Mathf.Pow(_navMeshAgent.stoppingDistance, 2); 
+        _animator.SetBool("CanAttack", _canAttack);
+        
     }
 
     private IEnumerator CheckForTarget()
