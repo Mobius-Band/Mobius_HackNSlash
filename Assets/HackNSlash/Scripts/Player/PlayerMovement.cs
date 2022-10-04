82767c5db75cee6247800be92130b920a4e0e83e
Choosecake <imchoosecake@gmail.com>
2022-10-04 10:16:37 -0300

    fixes player going through walls

--- Assets/HackNSlash/Scripts/Player/PlayerMovement.cs
+++ Assets/HackNSlash/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,8 +15,16 @@ namespace Player
         [SerializeField] private Animator _animator;
         public Vector2 MoveInput { get => _moveInput; set => _moveInput = value; }
         private Vector2 _moveInput;
+        private Vector3 _moveDirection;
+        private Rigidbody _rigidbody;
         private float _rotationVelocity;
-        private bool _canMove;
+        private bool _canMove = true;
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
         private void Update()
         {
             if (_moveInput == Vector2.zero | !_canMove)
@@ -22,15 +32,26 @@ namespace Player
                 _animator.SetBool("isMoving", false);
                 return;
             }
-            
+
             _animator.SetBool("isMoving", true);
-        
+
             float targetAngle = Mathf.Atan2(_moveInput.x, _moveInput.y) * Mathf.Rad2Deg + _cameraHolder.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationVelocity, _rotationTime/100);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _rotationVelocity,
+                _rotationTime / 100);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-        
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            transform.position += moveDirection.normalized * _moveSpeed * Time.deltaTime;
+
+            _moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            _moveDirection.Normalize();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_moveInput == Vector2.zero | !_canMove)
+            {
+                return;
+            }
+            
+            _rigidbody.velocity = _moveDirection * _moveSpeed; 
         }
 
         public void SuspendMovement()
